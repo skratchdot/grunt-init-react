64319d52e075285e38fecb6ec80b6871150fe3e0
skratchdot <git@skratchdot.com>
2016-01-05 18:23:51 -0500

    adding redux-devtools-filter-actions

--- example/src/app/containers/DevTools-dev.js
+++ example/src/app/containers/DevTools-dev.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { createDevTools } from 'redux-devtools';
-import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
+import FilterMonitor from 'redux-devtools-filter-actions';
+import LogMonitor from 'redux-devtools-log-monitor';
 
 // createDevTools takes a monitor and produces a DevTools component
 const DevTools = createDevTools(
-  <DockMonitor
-    toggleVisibilityKey='ctrl-h'
-    changePositionKey='ctrl-q'>
-      <LogMonitor theme='tomorrow' />
-  </DockMonitor>
+  <FilterMonitor>
+    <DockMonitor
+      toggleVisibilityKey="ctrl-h"
+      changePositionKey="ctrl-q"
+      fluid={true}
+      defaultSize={0.3}
+      defaultPosition="left"
+      defaultIsVisible={false}
+    >
+        <LogMonitor theme='tomorrow' />
+    </DockMonitor>
+  </FilterMonitor>
 );
 
 export default DevTools;
